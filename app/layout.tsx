89290bd4e9d91dfae19cb35bc01f8c0d15bfbd22
David Ramos Ruiz <dramos@squareup.com>
2023-06-09 05:12:03 +0200

    feat(home): :art: share layout across pages and include loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,22 @@ export default function RootLayout ({
 }) {
   return (
     <html lang='en'>
-      <body suppressHydrationWarning className={inter.className}>
+      <body suppressHydrationWarning className={`${inter.className} overflow-hidden`}>
         <Provider>
-          <main className='relative flex justify-center items-center flex-col max-w-7xl mx-auto sm:px-16 px-6;'>
-            <Navigation />
-            {children}
+          <Navigation />
+          <main className='relative flex justify-center items-center flex-col max-w-7xl mx-auto pb-32'>
+            <header>
+              <h1 className='mt-5 text-4xl font-extrabold leading-[1.15] text-black sm:text3xl text-center'>
+                Workfully Frontend Test
+                <br />
+                <span className='bg-gradient-to-r from-sky-600 via-cyan-500 to-teal-400 bg-clip-text text-transparent text-center'>
+                  Github Users Finder
+                </span>
+              </h1>
+            </header>
+            <section className='w-full max-w-md flex flex-col justify-center items-center mt-12 mx-auto  shadow-2xl bg-white p-10 rounded-lg'>
+              {children}
+            </section>
           </main>
         </Provider>
       </body>
