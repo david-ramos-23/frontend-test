97dc451d45072f75ec6c06709facbed3b617bad1
David Ramos Ruiz <dramos@squareup.com>
2023-06-09 05:12:03 +0200

    feat(home): :art: share layout across pages and include loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
-export default function Home () {
-  return (
-    <section className='w-full flex flex-col justify-center items-center'>
-      <h1 className='mt-5 text-4xl font-extrabold leading-[1.15] text-black sm:text3xl text-center'>
-        Workfully Frontend Test
-        <br />
-        <span className='bg-gradient-to-r from-sky-600 via-cyan-500 to-teal-400 bg-clip-text text-transparent text-center'>
-          Github Users Finder
-        </span>
-      </h1>
-    </section>
-  )
+
+export default async function Home () {
 }
