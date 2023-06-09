cceb24ed794be0224f64fa4c2ea3f4acd7e8f0e8
David Ramos Ruiz <dramos@squareup.com>
2023-06-09 05:03:44 +0200

    refactor(components): :recycle: `Avatar` and `Button`

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 
-export function Avatar ({ src, alt }: { src: string, alt: string }) {
+export function Avatar ({ src, alt, size = 40, className = 'rounded-full' }: { src: string, alt: string, className?: string, size?: number }) {
   return (
     <div className='flex items-center mr-2'>
-      <Image src={src} alt={alt} width={40} height={40} className='rounded-full' />
+      <Image src={src} alt={alt} width={size} height={size} className={className} />
     </div>
   )
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,11 @@
 export function Button ({ children, onClick, className, ...buttonProps }: React.ComponentProps<'button'>) {
   return (
-    <>
-      <button
-        onClick={onClick}
-        className={className}
-        {...buttonProps}
-      >
-        {children}
-      </button>
-    </>
+    <button
+      onClick={onClick}
+      className={className}
+      {...buttonProps}
+    >
+      {children}
+    </button>
   )
 }
